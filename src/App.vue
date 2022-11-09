bcff72f44e4adc8efc63bc1160c2fcb887109ce4
mattialicausi <licausivitomattia@gmail.com>
2022-11-09 15:08:59 +0100

    creato MainComponent contenente un titolo e importato in app.vue per la visualizzazione del titolo nel template

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,24 @@
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    
-    <div class="wrapper">
-      
-    </div>
-  </header>
 
-  <main>
-   
-  </main>
+ <MainComponent />
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<script>
+  import MainComponent from './components/MainComponent.vue';
+  export default {
+    components: {
+      MainComponent
+    },
+    data() {
+      return {
+        //return
+      }
+    }
+    
   }
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+<style lang="css">
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <main>
+        <h1>{{titolo}}</h1>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent',
+        data() {
+            return {
+                titolo: 'Vite Hello World'
+            }
+        }
+    }
+</script>
+
+<style lang="css">
+    main {
+        margin: 0 auto;
+    }
+
+    h1{
+        text-align: center;
+    }
+</style>
