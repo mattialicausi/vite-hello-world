9fa1dbec7da6263191a60578ebfefceeeffea431
mattialicausi <licausivitomattia@gmail.com>
2022-11-09 16:23:19 +0100

    aggiunto seconda img in jumbo

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -2,18 +2,25 @@
     <div class="hero">
         <div class="wrapper-content">
             <div class="header-hero">
-                <h1>Jumbotron</h1> 
+                <h1>{{titleHero}}</h1> 
             </div>
             <div class="main-hero">
-                <div class="nav-bar">
-                    <ul>
-                        <li>Topics</li>
-                        <li>Galleries</li>
-                        <li>Downloads</li>
-                        <li>About</li>
-                        <li>Follow Us</li>
-                    </ul>
+                <div class="container-left-side">
+                     <div class="nav-bar">
+                        <ul>
+                            <li>Topics</li>
+                            <li>Galleries</li>
+                            <li>Downloads</li>
+                            <li>About</li>
+                            <li>Follow Us</li>
+                        </ul>
+                    </div>
+
+                    <div class="container-left-img">
+                        <img class="img-left" src="../../public/images/horizontal.jpeg" alt="">
+                    </div>
                 </div>
+
                 <div class="container-img">
                     <img class="img-hero" src="../../public/images/space-nasa.jpeg" alt="img space">
                 </div>
@@ -27,7 +34,7 @@
         name: 'AppHero',
         data() {
             return {
-
+                titleHero: 'World photo'
             }
         }
         
@@ -57,8 +64,23 @@
         display: flex;
     }
 
-    .nav-bar{
+    .container-left-side{
         width: 40%;
+        height: 80vh;
+    }
+
+    .nav-bar{
+        height: 30%;
+    }
+
+    .container-left-img{
+        height: 70%;
+    }
+
+    .img-left{
+        width: 100%;
+        height: auto;
+        margin: 0 auto;
     }
 
     .container-img{
